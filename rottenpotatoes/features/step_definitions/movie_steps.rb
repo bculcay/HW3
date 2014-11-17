7bb880846e505352f10629af36f3bbb8507be1eb
saasbook <saasbook@saasbook.(none)>
2014-11-16 21:26:03 -0400

    HW3 Complete

--- rottenpotatoes/features/step_definitions/movie_steps.rb
+++ rottenpotatoes/features/step_definitions/movie_steps.rb
@@ -2,10 +2,11 @@
 
 Given /the following movies exist/ do |movies_table|
   movies_table.hashes.each do |movie|
+    Movie.create!(movie)
     # each returned element will be a hash whose key is the table header.
     # you should arrange to add that movie to the database here.
   end
-  flunk "Unimplemented"
+  #assert false, "Unimplmemented"
 end
 
 # Make sure that one string (regexp) occurs before or after another one
@@ -13,22 +14,44 @@ end
 
 Then /I should see "(.*)" before "(.*)"/ do |e1, e2|
   #  ensure that that e1 occurs before e2.
-  #  page.body is the entire content of the page as a string.
-  flunk "Unimplemented"
+  #  page.content  is the entire content of the page as a string.
+  x = page.body.split(e1)
+  assert x.length == 3 # before title, before more info link, other content
+  x = x[2].split(e2)
+  assert x.length == 3
 end
 
 # Make it easier to express checking or unchecking several boxes at once
 #  "When I uncheck the following ratings: PG, G, R"
 #  "When I check the following ratings: G"
 
+#When /I (un)?check the following ratings: (.*)/ do |uncheck, rating_list|
 When /I (un)?check the following ratings: (.*)/ do |uncheck, rating_list|
   # HINT: use String#split to split up the rating_list, then
   #   iterate over the ratings and reuse the "When I check..." or
   #   "When I uncheck..." steps in lines 89-95 of web_steps.rb
-  flunk "Unimplemented"
+  
+  rating_list.split(%r{\s*,\s*}).each_with_index {
+      |name, index|
+        if(!index) #first element
+          When %Q{I #{uncheck}check "ratings[#{name}]"}
+        else
+          And %Q{I #{uncheck}check "ratings[#{name}]"}
+        end
+    }
 end
 
-Then /I should see all the movies/ do
-  # Make sure that all the movies in the app are visible in the table
-  flunk "Unimplemented"
+#Then /I should( not)? see all of the movies: (.*)/ do |ifnot, movies_list|
+Then /I should( not)? see all of the movies/ do |orNot|
+  Movie.all.each_with_index {
+    |movie, index|
+      name = movie[:title]
+      if(!index) #first element
+        Then %Q{I should#{orNot} see "#{name}"}
+      else
+        And %Q{I should#{orNot} see "#{name}"}
+      end
+  }
 end
+
+
